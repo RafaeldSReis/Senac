f09c8a4e5e090defd2469f1ab91cd011de26533c
rafael reis <rafael.reis.git@gmail.com>
2022-04-12 13:10:05 -0300

    Curso senac 12/04/2022

---
+++ Professor-Gustavo - C#/Projeto_modelo_22/Projeto_modelo_22/TelaPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Projeto_modelo_22
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exerciciosDeLógicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logica1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logica2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logica3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bancoDeDadosSQLServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exerciciosDeLógicaToolStripMenuItem,
+            this.bancoDeDadosSQLServerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exerciciosDeLógicaToolStripMenuItem
+            // 
+            this.exerciciosDeLógicaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logica1ToolStripMenuItem,
+            this.logica2ToolStripMenuItem,
+            this.logica3ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.sairToolStripMenuItem});
+            this.exerciciosDeLógicaToolStripMenuItem.Name = "exerciciosDeLógicaToolStripMenuItem";
+            this.exerciciosDeLógicaToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.exerciciosDeLógicaToolStripMenuItem.Text = "Exercicios de Lógica";            
+            // 
+            // logica1ToolStripMenuItem
+            // 
+            this.logica1ToolStripMenuItem.Name = "logica1ToolStripMenuItem";
+            this.logica1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logica1ToolStripMenuItem.Text = "Logica 1";
+            this.logica1ToolStripMenuItem.Click += new System.EventHandler(this.logica1ToolStripMenuItem_Click);
+            // 
+            // logica2ToolStripMenuItem
+            // 
+            this.logica2ToolStripMenuItem.Name = "logica2ToolStripMenuItem";
+            this.logica2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logica2ToolStripMenuItem.Text = "Logica 2";
+            // 
+            // logica3ToolStripMenuItem
+            // 
+            this.logica3ToolStripMenuItem.Name = "logica3ToolStripMenuItem";
+            this.logica3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logica3ToolStripMenuItem.Text = "Logica 3";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // bancoDeDadosSQLServerToolStripMenuItem
+            // 
+            this.bancoDeDadosSQLServerToolStripMenuItem.Name = "bancoDeDadosSQLServerToolStripMenuItem";
+            this.bancoDeDadosSQLServerToolStripMenuItem.Size = new System.Drawing.Size(162, 20);
+            this.bancoDeDadosSQLServerToolStripMenuItem.Text = "Banco de dados SQL Server";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMenu";
+            this.Text = "Tela Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem exerciciosDeLógicaToolStripMenuItem;
+        private ToolStripMenuItem logica1ToolStripMenuItem;
+        private ToolStripMenuItem logica2ToolStripMenuItem;
+        private ToolStripMenuItem logica3ToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem sairToolStripMenuItem;
+        private ToolStripMenuItem bancoDeDadosSQLServerToolStripMenuItem;
+    }
+}
---
+++ Professor-Gustavo - C#/Projeto_modelo_22/Projeto_modelo_22/TelaPrincipal.cs
@@ -0,0 +1,23 @@
+namespace Projeto_modelo_22
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();        }
+
+       
+        private void logica1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /* MessageBox.Show("Módulo em produção...", "Faz o PIX que fica pronto", 
+              MessageBoxButtons.OK, MessageBoxIcon.Information);*/
+
+            // crindo um objeto da classe frmLogica 1
+            frmLogica1 frm = new frmLogica1();
+            // chamando o método que mostra o objeto na tela
+
+            // frm.Show();
+            frm.ShowDialog();
+        }
+    }
+}
