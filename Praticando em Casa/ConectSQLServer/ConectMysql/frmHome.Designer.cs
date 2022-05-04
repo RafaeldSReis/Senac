36d50fb718a5c8dea4ae3e843830b530bbedf195
rafael reis <rafael.reis.git@gmail.com>
2022-05-04 13:12:33 -0300

    Pratica em casa Login com SQLServer

--- Praticando em Casa/ConectSQLServer/ConectMysql/frmHome.Designer.cs
+++ Praticando em Casa/ConectSQLServer/ConectMysql/frmHome.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Nome = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.ltsList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(81, 113);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 0;
+            this.Nome.Text = "Nome";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(134, 110);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Telefone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(341, 110);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(116, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // ltsList
+            // 
+            this.ltsList.FormattingEnabled = true;
+            this.ltsList.Location = new System.Drawing.Point(531, 110);
+            this.ltsList.Name = "ltsList";
+            this.ltsList.Size = new System.Drawing.Size(424, 420);
+            this.ltsList.TabIndex = 4;
+            this.ltsList.SelectedIndexChanged += new System.EventHandler(this.ltsList_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(134, 186);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(134, 254);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(134, 327);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1321, 644);
+            this.ClientSize = new System.Drawing.Size(1064, 644);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.ltsList);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.Nome);
             this.Name = "frmHome";
             this.Text = "Home";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ListBox ltsList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- Praticando em Casa/ConectSQLServer/ConectMysql/frmHome.cs
+++ Praticando em Casa/ConectSQLServer/ConectMysql/frmHome.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ namespace ConectMysql
 {
     public partial class frmHome : Form
     {
+        private SqlConnection conexao;
+        private string strSQL;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
         public frmHome()
         {
             InitializeComponent();
@@ -20,6 +26,68 @@ namespace ConectMysql
         private void sairToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Criar conexão com SQLServer               
+                conexao = new SqlConnection(@"Data Source=DESKTOP-KRLKM9V\SQLEXPRESS;Initial Catalog=csharp;User ID=sa;Password=senac");
+
+                // Inserindo usuario
+                strSQL = "INSERT INTO contact (name, phone) VALUES (@NAME, @PHONE)";
+                command = new SqlCommand(strSQL, conexao);
+                command.Parameters.AddWithValue("@NAME", txtName.Text);
+                command.Parameters.AddWithValue("@PHONE", txtPhone.Text);
+                // abrindo conexao e executando o comando
+                conexao.Open();
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastrado com Sucesso!!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                command.Clone();
+                conexao = null;
+                command = null;
+            }
+        }
+       
+        private void ltsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            conexao = new SqlConnection(@"Data Source=DESKTOP-KRLKM9V\SQLEXPRESS;Initial Catalog=csharp;User ID=sa;Password=senac");
+
+
+            string query = "SELECT * FROM contact";
+            SqlCommand dt = new SqlCommand(query, conexao);            
+
+            try
+            {
+                conexao.Open();
+                reader = dt.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    string name = reader.GetString("name");
+                    ltsList.Items.Add(name);
+
+                }
+                conexao.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
         }
     }
 }
