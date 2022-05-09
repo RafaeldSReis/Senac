42d619990d5f131162da98627ecff0590f33155a
rafael reis <rafael.reis.git@gmail.com>
2022-05-09 14:09:00 -0300

    Pratica Conectando a API Cotação

--- praticando/Crud_SQLServer/Crud/Crud/Home.Designer.cs
+++ praticando/Crud_SQLServer/Crud/Crud/Home.Designer.cs
@@ -34,6 +34,7 @@
             this.aPIViaCepCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aPICotaçãoCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,6 +53,7 @@
             this.programasPraticaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.agendaCSQLServerToolStripMenuItem,
             this.aPIViaCepCToolStripMenuItem,
+            this.aPICotaçãoCToolStripMenuItem,
             this.toolStripSeparator1,
             this.sairToolStripMenuItem});
             this.programasPraticaToolStripMenuItem.Name = "programasPraticaToolStripMenuItem";
@@ -84,15 +86,24 @@
             this.sairToolStripMenuItem.Text = "Sair";
             this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
             // 
+            // aPICotaçãoCToolStripMenuItem
+            // 
+            this.aPICotaçãoCToolStripMenuItem.Name = "aPICotaçãoCToolStripMenuItem";
+            this.aPICotaçãoCToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.aPICotaçãoCToolStripMenuItem.Text = "API Cotação C#";
+            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmHome";
             this.Text = "Home";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -109,5 +120,6 @@
         private System.Windows.Forms.ToolStripMenuItem aPIViaCepCToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aPICotaçãoCToolStripMenuItem;
     }
 }
