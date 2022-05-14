a6ab548d9f18f9f190f13d05f94b90d45807cdd6
rafael reis <rafael.reis.git@gmail.com>
2022-05-14 20:40:10 -0300

    Atualizando

--- praticando/Crud_SQLServer/Servidor/Agenda C#/Comandos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(ID int IDENTITY(1,1) PRIMARY KEY, name varchar (50) NOT NULL, passwd varchar(50) NOT NULL);  
-
-SELECT * FROM contact;
-
-SELECT * FROM users;
-
-TRUNCATE TABLE contact; -- zera a tabela
